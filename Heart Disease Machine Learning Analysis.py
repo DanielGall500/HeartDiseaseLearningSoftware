49d6868e00da62e6970521ec9db0f307a4de83e9
DannyGotTea <danielgallagher500@gmail.com>
2016-04-05 13:08:37 -0700

    Principal Component Analysis Complete

--- Heart Disease Machine Learning Analysis.py
+++ Heart Disease Machine Learning Analysis.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
+from sklearn.linear_model import LinearRegression
 
 csv_loc = "C:/Users/dano/Desktop/Heart Disease Data/processed_hungarian_data.csv"
 
 data = pd.read_csv(csv_loc, sep=';')
+data = data.replace('?',0)
 
 colours = np.random.rand(50, 50)
 
@@ -16,21 +18,62 @@ def process_array(convert_array):
 	new_array = np.array([])
 	for i in convert_array:
 		if i != '?':
-			new_array = np.append(new_array, float(i))
+			value = np.array([float(i)])
+			new_array = np.concatenate((new_array,value))
 		else:
-			new_array = np.append(new_array, 0)
-	print "NEW ARRAY:", new_array
+			new_array = np.concatenate((new_array, [0])) #NEED TO DELETE BEFORE ML
 	return new_array
 
-
-
+def process_whole_array(dataframe):
+	for row in dataframe:
+		dataframe[row].apply(lambda x: float(x))
+	return dataframe
+			
+"""
 blood_press = process_array(data['trestbps'])
 ages = process_array(data['age'])
+genders = process_array(data['gender'])
+chest_pain = process_array(data['cp'])
+cholestoral = process_array(data['chol'])
+blood_sugar = process_array(data['fbs'])
+resting_ecg = process_array(data['restecg'])
+max_heartrate = process_array(data['thalach'])
+exercise_angina = process_array(data['exang'])
+st_depression = process_array(data['oldpeak'])
+"""
+processed_data = process_whole_array(data)
+print processed_data
 
-print "Blood Pressure:", blood_press
-print "Ages:", ages
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
-plt.scatter(blood_press, ages, s=10, alpha=0.5)
+pca_2 = PCA(2)
+
+plot_columns = pca_2.fit_transform(processed_data)
+
+plt.scatter(plot_columns[:,0],plot_columns[:,1])
+
+"""
+ages = ages.reshape(len(ages),1)
+blood_press = blood_press.reshape(len(blood_press),1)
+
+regression = LinearRegression()
+regression.fit(ages, blood_press)
+
+colour_one = "b"
+
+for age, bp in zip(ages, blood_press):
+	plt.scatter(ages, blood_press, color=colour_one)
+"""
+plt.title("Heart Disease")
+
+plt.grid(alpha=0.4)
+
+try:
+	plt.plot(blood_press, reg.predict(blood_press))
+except NameError:
+	print "Regression NameError"
+
 plt.show()
 
 
