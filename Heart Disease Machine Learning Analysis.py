f26ea21e0a752990aa911a8299fbb08f2ba048ac
DannyGotTea <danielgallagher500@gmail.com>
2016-04-05 10:41:37 -0700

    Fixed plotting

--- Heart Disease Machine Learning Analysis.py
+++ Heart Disease Machine Learning Analysis.py
@@ -8,7 +8,7 @@ csv_loc = "C:/Users/dano/Desktop/Heart Disease Data/processed_hungarian_data.csv
 
 data = pd.read_csv(csv_loc, sep=';')
 
-colours = np.random.rand(50, 50, 0)
+colours = np.random.rand(50, 50)
 
 area = np.pi * (15 * np.random.rand(50)) ** 2 #0 to 15 point radiuses
 
@@ -16,7 +16,10 @@ def process_array(convert_array):
 	new_array = np.array([])
 	for i in convert_array:
 		if i != '?':
-			np.append(new_array, float(i))
+			new_array = np.append(new_array, float(i))
+		else:
+			new_array = np.append(new_array, 0)
+	print "NEW ARRAY:", new_array
 	return new_array
 
 
@@ -24,7 +27,10 @@ def process_array(convert_array):
 blood_press = process_array(data['trestbps'])
 ages = process_array(data['age'])
 
-plt.scatter(blood_press, ages, s=100, alpha=0.5)
+print "Blood Pressure:", blood_press
+print "Ages:", ages
+
+plt.scatter(blood_press, ages, s=10, alpha=0.5)
 plt.show()
 
 
