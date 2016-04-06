2cee2c528063470ec473df4600c0f6b246325f6a
DannyGotTea <danielgallagher500@gmail.com>
2016-04-06 12:55:15 -0700

    Support Vector Machine Implementation

--- Heart Disease Machine Learning Analysis.py
+++ Heart Disease Machine Learning Analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
-from sklearn.linear_model import LinearRegression
+from sklearn import svm
 
 csv_loc = "C:/Users/dano/Desktop/Heart Disease Data/processed_hungarian_data.csv"
 
@@ -42,37 +42,46 @@ exercise_angina = process_array(data['exang'])
 st_depression = process_array(data['oldpeak'])
 """
 processed_data = process_whole_array(data)
-print processed_data
+
+feature_names = ['age','gender','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak']
+label_names = ['num']
+
+data_features = processed_data[feature_names]
+data_labels = processed_data['num']
+
+label_list = []
+for i in data_labels:
+	label_list.append(i)
+	print "AP:", i
 
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
 
 pca_2 = PCA(2)
 
 plot_columns = pca_2.fit_transform(processed_data)
 
-plt.scatter(plot_columns[:,0],plot_columns[:,1])
+#plt.scatter(plot_columns[:,0],plot_columns[:,1])
 
-"""
-ages = ages.reshape(len(ages),1)
-blood_press = blood_press.reshape(len(blood_press),1)
+from sklearn.cross_validation import train_test_split
 
-regression = LinearRegression()
-regression.fit(ages, blood_press)
+features_train, features_test, labels_train, labels_test = train_test_split(data_features, label_list, test_size=0.33, random_state=42)
 
-colour_one = "b"
+clf = svm.SVC()
 
-for age, bp in zip(ages, blood_press):
-	plt.scatter(ages, blood_press, color=colour_one)
-"""
+print features_train
+print labels_train
+
+clf.fit(features_train,labels_train)
+
+predictions = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print "Accuracy Score:", accuracy_score(labels_test, predictions)
+
+
 plt.title("Heart Disease")
 
 plt.grid(alpha=0.4)
-
-try:
-	plt.plot(blood_press, reg.predict(blood_press))
-except NameError:
-	print "Regression NameError"
 
 plt.show()
 
